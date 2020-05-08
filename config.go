fcbf4ed5901ff18b773b73744ad1ccc282564b5c
inokappa <inokara@gmail.com>
2020-05-08 22:26:01 +0900

    update

--- config.go
+++ config.go
@@ -11,9 +11,14 @@ type Config struct {
 	common.PackerConfig    `mapstructure:",squash"`
 	awscommon.AccessConfig `mapstructure:",squash"`
 
-	TemplateId            string `mapstructure:"template_id"`
-	SourceTemplateVersion string `mapstructure:"source_template_version"`
-	VersionDescription    string `mapstructure:"version_description"`
+	// TemplateId            string `mapstructure:"template_id"`
+	// SourceTemplateVersion string `mapstructure:"source_template_version"`
+	// VersionDescription    string `mapstructure:"version_description"`
+	Templates []struct {
+		ID                 string `mapstructure:"id"`
+		SourceVersion      string `mapstructure:"source_version"`
+		VersionDescripiton string `mapstructure:"version_descripiton"`
+	} `mapstructure:"templates"`
 
 	ctx interpolate.Context
 }
